9d8fa1688e408ee2f3ae404e3bb947e587e4ff36
Kanishk <kr.kanishk@gmail.com>
2018-08-17 12:41:21 +0530

    Adding Taking Screenshot code

--- Project/src/TakingScreenShotSelenium/Navigation.java
+++ Project/src/TakingScreenShotSelenium/Navigation.java
@@ -1,45 +1,27 @@
- package CheckGmail;
+package TakingScreenShotSelenium;
 
 import java.util.List;
-
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Check 
+public class Navigation 
 {
 	WebDriver driver;
-	
-	public void check() throws InterruptedException
+	TakeScreens scr;
+	public Navigation()
 	{
 		System.setProperty("webdriver.chrome.driver","D://chromedriver.exe");
 		driver = new ChromeDriver();
-		
 		driver.get("https://www.guru99.com/");
-		
+		scr.capture();
 		List<WebElement> elements = driver.findElements(By.xpath("//span[@class='titreck']"));
 		for(WebElement ele : elements)
 		{
 			ele.findElement(By.xpath("//span[text() = 'SAP']")).click();
 			break;
 		}
-		
-		driver.findElement(By.xpath("//span[text() = 'SAP - FICO']")).click();
-
-		driver.switchTo().frame(1);
-		
-
-		StringBuilder str = new StringBuilder();
-		str.setLength(10);
-		str.append("kanishk");
-		str.append("another");
-		str.append("another programmer");
-		
-		driver.navigate().back();
-		
-	} 
-
+	}
 }
-
